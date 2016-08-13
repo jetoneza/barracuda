385c4145dc321d0c509d107d088a7d3e896046ca
Jethro Ordaneza <jet.oneza@gmail.com>
2016-08-13 19:31:15 +0800

    Setup bootstrap loader

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 export const HomeView = () => (
-    <div>
+    <div className="home">
+      <div className="sample-box"></div>
+      <div className="btn btn-success btn-sm">Label</div>
       <h4>Kung unsa man imong ma huna hunaan, atuang buhaton! Imagination ang limit!</h4>
     </div>
 )
