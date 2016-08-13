de240d5694efd6fcaa84c85b50e67dc70523f7e0
Jethro Ordaneza <jet.oneza@gmail.com>
2016-08-13 21:38:04 +0800

    Add color themes

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -4,12 +4,17 @@ class HomeView extends Component {
   render() {
     return (
         <div className="home text-center">
-          <h1 className="title">Barracuda</h1>
+          <div className="theme-box-group">
+            <div className="box box-1">Ba</div>
+            <div className="box box-2">Ra</div>
+            <div className="box box-3">Cu</div>
+            <div className="box box-4">Da</div>
+          </div>
           <div className="input-group">
             <input type="text" className="form-control"/>
           </div>
           <div className="actions">
-            <div className="btn btn-default">Barracuda Search</div>
+            <div className="btn btn-primary btn-search">Barracuda Search</div>
           </div>
         </div>
     );
